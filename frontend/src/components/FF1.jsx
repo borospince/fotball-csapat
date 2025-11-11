1d76652f9073972c0ba24207e89128807066d175
borospince <pinczesi.mark@szbiszeged.hu>
2025-11-11 15:36:27 +0100

    2025.11.11

--- frontend/src/components/FF1.jsx
+++ frontend/src/components/FF1.jsx
@@ -1,15 +1,24 @@
 import './ff1.css';
 
-const FF1 = ({ff1}) => {
+const FF1 = ({ ff1 }) => {
   return (
-    <div className="tartalom-kontener">
-        <h1>{ff1.cim}</h1>
-        <p>{ff1.tartalom}</p>
-        <div className="ff1-kep-kontener">
-            <img src={ff1.kep} alt={ff1.kep} />
+    <div className="ff1-card">
+      <div className="ff1-kep-kontener">
+        <img src={ff1.kep} alt={ff1.nev} />
+      </div>
+      <div className="ff1-adatok">
+        <h2 className="ff1-nev">{ff1.nev}</h2>
+        <p className="ff1-nemzetiseg">{ff1.nemzetiseg}</p>
+        <p className="ff1-szuletes">Született: {ff1.szuletes}</p>
+        <p className="ff1-poszt">Poszt: {ff1.poszt}</p>
+        <div className="ff1-stats">
+          <p>Ár: {ff1.ar}</p>
+          <p>Gól: {ff1.gol}</p>
+          <p>Gólpassz: {ff1.golpassz}</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default FF1;
--- frontend/src/components/U19.jsx
+++ frontend/src/components/U19.jsx
@@ -1,15 +1,26 @@
 import './u19.css';
 
-const U19 = ({u19}) => {
+const U19 = ({ u19 }) => {
   return (
-    <div className="tartalom-kontener">
-        <h1>{u19.cim}</h1>
-        <p>{u19.tartalom}</p>
-        <div className="u19-kep-kontener">
-            <img src={u19.kep} alt={u19.kep} />
-        </div>
+    <div className="u19-card">
+      <div className="u19-kep-kontener">
+        <img src={u19.kep} alt={u19.nev} />
+      </div>
+
+      <div className="u19-adatok">
+        <p className="u19-nev">{u19.nev}</p>
+        <p className="u19-nemzetiseg">{u19.nemzetiseg}</p>
+        <p className="u19-szuletes">{u19.szuletes}</p>
+        <p className="u19-poszt">{u19.poszt}</p>
+      </div>
+
+      <div className="u19-stats">
+        <p>Ár: {u19.ar}</p>
+        <p>Gól: {u19.gol}</p>
+        <p>Gólpassz: {u19.golpassz}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default U19;
