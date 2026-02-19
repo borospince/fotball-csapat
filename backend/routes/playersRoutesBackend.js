072fae13ce6cc581538b1ab942cf4d24778cb1a1
borospince <pinczesi.mark@szbiszeged.hu>
2026-02-19 14:46:28 +0100

    2026.02.19

--- backend/routes/playersRoutesBackend.js
+++ backend/routes/playersRoutesBackend.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const pictureDeleter = require('../middlewares/pictureDelete');
 const {
-    deleteOneUserBackend, getAllPlayersBackend
+    deleteOneUserBackend, getAllPlayersBackend, getOneUserBackend
 } = require('../controllers/playerControllersBackend');
 
 const router = express.Router();
 
 router.get('/', getAllPlayersBackend);
-// router.get('/:id',getOneUserBackend);
+router.get('/:id',getOneUserBackend);
 // router.get('/',postUserBackend);
 // router.get('/modosit/:id',updateOneUserBackend);
 router.delete('/:id', pictureDeleter ,deleteOneUserBackend);
